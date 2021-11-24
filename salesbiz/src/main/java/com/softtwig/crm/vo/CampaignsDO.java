9abb36c1010b8ced1ed97abf07c08c4b5fcb232f
YUVARANI SELLAIYA <yuva.techboot@gmail.com>
2021-11-24 13:26:46 +0530

    Update CampaignsDO.java

--- salesbiz/src/main/java/com/softtwig/crm/vo/CampaignsDO.java
+++ salesbiz/src/main/java/com/softtwig/crm/vo/CampaignsDO.java
@@ -1,6 +1,7 @@
 package com.softtwig.crm.vo;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -21,6 +22,19 @@ public class CampaignsDO implements Serializable{
 	private static final long serialVersionUID = -218430471859463936L;
 	
 	private int campaignId;
+	private String campaignOwner;
+	private String campaignName;
+	private long mobileNo;
+	private String type;
+	private String status;
+	private Date startDate;
+	private Date endDate;
+	private double expectedRevenue;
+	private double actualCost;
+	private double budgetedCost;
+	private double expectedResponse;
+	private int numberSent;
+	private String Description;
 	
 	@Id
 	@GeneratedValue
@@ -32,5 +46,109 @@ public class CampaignsDO implements Serializable{
 	public void setCampaignId(int campaignId) {
 		this.campaignId = campaignId;
 	}
+	@Column(name = "CAMPAIGN_OWNER")
+	public String getCampaignOwner() {
+		return campaignOwner;
+	}
 
+	public void setCampaignOwner(String campaignOwner) {
+		this.campaignOwner = campaignOwner;
+	}
+	@Column(name = "CAMPAIGN_NAME")
+	public String getCampaignName() {
+		return campaignName;
+	}
+
+	public void setCampaignName(String campaignName) {
+		this.campaignName = campaignName;
+	}
+	@Column(name = "MOBILE_NO")
+	public long getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(long mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	@Column(name = "CAMPAIGN_TYPE")
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	@Column(name = "START_DATE")
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	@Column(name = "END_DATE")
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public double getExpectedRevenue() {
+		return expectedRevenue;
+	}
+
+	public void setExpectedRevenue(double expectedRevenue) {
+		this.expectedRevenue = expectedRevenue;
+	}
+
+	public double getActualCost() {
+		return actualCost;
+	}
+
+	public void setActualCost(double actualCost) {
+		this.actualCost = actualCost;
+	}
+
+	public double getBudgetedCost() {
+		return budgetedCost;
+	}
+
+	public void setBudgetedCost(double budgetedCost) {
+		this.budgetedCost = budgetedCost;
+	}
+	
+	public double getExpectedResponse() {
+		return expectedResponse;
+	}
+
+	public void setExpectedResponse(double expectedResponse) {
+		this.expectedResponse = expectedResponse;
+	}
+	@Column(name = "NUMBER_SENT")
+	public int getNumberSent() {
+		return numberSent;
+	}
+
+	public void setNumberSent(int numberSent) {
+		this.numberSent = numberSent;
+	}
+	@Column(name = "DESCRIPTION")
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+    
 }
